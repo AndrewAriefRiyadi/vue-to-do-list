ad309a2ef98882d75d99a60079fcf2612f0d1505
AndrewAriefRiyadi <andrewariefr@gmail.com>
2025-08-02 13:18:55 +0800

    working slay button, until completed. Next API Integration

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <script setup>
-  import { ref } from 'vue'
-  import modalCreate from './components/modalCreate.vue'
-  import itemList from './components/itemList.vue'
+import { ref } from 'vue'
+import modalCreate from './components/modalCreate.vue'
+import itemList from './components/itemList.vue'
 
-  const isModalOpen = ref(false)
+const isModalOpen = ref(false);
+const completed = ref(false);
 </script>
 
 <template>
+
   <body class="bg-gray-900 font-mono py-28 lg:px-64 md:px-48">
     <div class="h-screen flex flex-col gap-4">
       <p class="text-white text-2xl text-center">To-Slay List</p>
       <!-- Buttons -->
       <div class="flex justify-around">
-        <button @click="isModalOpen = true" class="w-fit px-3 py-1 bg-green-200 rounded-2xl">Create</button>
+        <button @click="isModalOpen = true"
+          class="w-fit px-3 py-1 outline-1 text-white outline-gray-700 bg-green-800 rounded-2xl hover:bg-green-700 hover:outline-gray-900 transition">Create</button>
       </div>
-      <itemList task="Tes" ></itemList>
+      <itemList :task="'Bunuh Goblin'" :completed="completed" @update:completed="completed = $event" />
     </div>
     <!-- Modal Create -->
     <modalCreate :visible="isModalOpen" @close="isModalOpen = false" />
   </body>
-  
 </template>
--- src/components/itemList.vue
+++ src/components/itemList.vue
@@ -3,17 +3,55 @@ import { ref } from 'vue';
 
 const props = defineProps({
     task: String,
-    completed: Boolean
+    completed: Boolean,
 })
 
+const health = ref(5) // ✅ health lokal
+const emit = defineEmits(['update:completed'])
 
+function swordClick() {
+    if (props.completed) return;
 
+    health.value--;
 
+    if (health.value <= 0) {
+        emit('update:completed', true)
+    }
+}
 </script>
 
 <template>
-    <div class=" w-full outline-1 outline-white text-white py-2 px-4">
-        {{ task }}
+    <div class=" flex">
+        <div
+            class=" w-full outline-1 outline-gray-700 bg-gray-800 text-white py-2 px-4 rounded-l hover:bg-gray-900 hover:outline-gray-500 transition">
+            <div class=" flex justify-between">
+                <p>{{ task }}</p>
+                <div>
+                    <span v-if="completed == true" class="text-green-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414L9 13.414l4.707-4.707z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </span>
+                    <span v-else class="text-gray-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M10 18a8 8 0 100-16 8 8 0 000 16zM10 16a6 6 0 110-12 6 6 0 010 12z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div
+            class=" p-2 outline-1 outline-gray-700 bg-gray-800 text-white py-2 px-4 rounded-r hover:bg-gray-900 hover:outline-gray-500 transition" @click="swordClick()">
+            <span class="select-none pointer-events-none">
+                🗡️
+            </span>
+        </div>
     </div>
 
 </template>
